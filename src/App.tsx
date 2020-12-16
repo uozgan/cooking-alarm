93c8b1fa6a15dbd1a51b3149474827b88a89ca75
uozgan <utkuozgan_8@hotmail.com>
2020-12-16 22:56:57 +0100

    Functioning cooking alarm

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,9 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import ButtonSet from './Components/ButtonSet';
 import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      height: "100%",
-      margin: "100px",
-      textAlign: "center",
-    },
-    inputStyle: {
-      textAlign: 'center',
-      display: "flex",
-      justifyContent: "center",
-    },
     container: {
       border: "solid 5px gray",
       display: "flex",
@@ -27,13 +16,14 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: "center",
     },
     timeWrapper: {
-      width: "50%",
+      width: "60%",
       border : "solid 3px blue",
       display: "flex",
       textAlign: "center",
       flexDirection: "row",
       justifyContent: "center",
       alignItems: "center",
+      fontSize: "25px",
     }
   }),
 );
@@ -52,15 +42,15 @@ console.log("Target Time", targetTime);
 // console.log("Second", seconds);
 
   return (
-    <Box height={1} width={1} textAlign="center">
-      <Grid container className={classes.container}>
+    <Box height={1} textAlign="center" margin="100px">
+    <Grid container className={classes.container}>
     <h3>Start your cooking time</h3>
     <Grid container justify="space-evenly" className={classes.timeWrapper}>
-    <Input type="number" value = {hours < 10 ? `0${hours}` : hours} onChange = {e => setTargetTime((Number(e.target.value) * 3600) + (minutes * 60) + seconds)} />
+    <Input style={{width:"65px", fontSize:"40px"}} type="number" value = {hours < 10 ? `0${hours}` : hours} onChange = {e => setTargetTime((Number(e.target.value) * 3600) + (minutes * 60) + seconds)} />
     <span>:</span>
-    <Input type="number" value = {minutes < 10 ? `0${minutes}` : minutes} onChange={e => (Number(e.target.value) > 60) ? null : setTargetTime((hours * 3600) + (Number(e.target.value) * 60) + seconds)} />
+    <Input style={{width:"65px", fontSize:"40px"}} type="number" value = {minutes < 10 ? `0${minutes}` : minutes} onChange={e => (Number(e.target.value) > 60) ? null : setTargetTime((hours * 3600) + (Number(e.target.value) * 60) + seconds)} />
     <span>:</span>
-  <TextField style={{width:"40px", fontSize:"40px"}} type="number" value = {seconds < 10 ? `0${seconds}` : seconds} onChange={e => (Number(e.target.value) > 60) ? null : setTargetTime((hours * 3600) + (minutes * 60) + Number(e.target.value))} />
+    <Input style={{width:"65px", fontSize:"40px"}} type="number" value = {seconds < 10 ? `0${seconds}` : seconds} onChange={e => (Number(e.target.value) > 60) ? null : setTargetTime((hours * 3600) + (minutes * 60) + Number(e.target.value))} />
     </Grid>
     </Grid>
     <ButtonSet targetTime={targetTime} setTargetTime={setTargetTime}/>
